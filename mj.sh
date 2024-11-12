2bd80b79ae4e9d02994f0aaecf88afa9ba7ebc8f
Kyle Long <kyle.long2048@gmail.com>
2024-11-12 19:35:45 +1100

    update

--- mj.sh
+++ mj.sh
@@ -3,25 +3,41 @@
 ### Requires: brew install cliclick
 
 # Interval in seconds
-INTERVAL=298
+INTERVAL=270
 
 # Movement in pixels
 MOVE_DISTANCE=1
+
+# Function to get the current position
+get_position() {
+  POSITION=$(cliclick p:)
+  X=$(echo $POSITION | cut -d, -f1 | cut -d: -f2)
+  Y=$(echo $POSITION | cut -d, -f2)
+}
+
+# Get initial  position
+get_position
+LAST_X=$X
+LAST_Y=$Y
 
 echo "Starting mj..."
 
 # Infinite loop 
 while true; do
-  # Save position
-  POSITION=$(cliclick p:)
-  X=$(echo $POSITION | cut -d, -f1 | cut -d: -f2)
-  Y=$(echo $POSITION | cut -d, -f2)
-
-  # move
-  cliclick m:$(($X + MOVE_DISTANCE)),$Y
-  sleep 0.0001
-  cliclick m:$X,$Y
-
-  # Wait for the specified interval
+  # Wait for the inactivity interval
   sleep $INTERVAL
+  
+  # Get the current  position
+  get_position
+  
+  # Check if moved since the last check
+  if [[ "$X" == "$LAST_X" && "$Y" == "$LAST_Y" ]]; then
+    cliclick m:$(($X + MOVE_DISTANCE)),$Y
+    sleep 0.001
+    cliclick m:$X,$Y
+  fi
+
+  # Update last known position
+  LAST_X=$X
+  LAST_Y=$Y
 done
